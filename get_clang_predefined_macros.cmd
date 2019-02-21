ec1dea71b784cfe1a0bf0d58052300b655ceadb1
degski <degski@gmail.com>
2019-02-21 05:08:24 +0200

    init

---
+++ get_clang_predefined_macros.cmd
@@ -0,0 +1,2 @@
+echo | clang -dM -E -
+@ pause
---
+++ install.cmd
@@ -0,0 +1,5 @@
+@ xcopy *.props "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VC\VCTargets\" /S /Q /K /Y > NUL
+@ xcopy *.targets "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VC\VCTargets\" /S /Q /K /Y > NUL
+@ if exist "C:\Program Files\LLVM\msbuild-bin" (rmdir /S /Q "C:\Program Files\LLVM\msbuild-bin")
+@ md "C:\Program Files\LLVM\msbuild-bin"
+@ copy "C:\Program Files\LLVM\bin\clang++.exe" "C:\Program Files\LLVM\msbuild-bin\cl.exe" > NUL
